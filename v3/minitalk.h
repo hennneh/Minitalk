6f7c285b987aaee70dcd3360f7dccf1b315afa13
hennneh <hendrik.lehmann96@gmail.com>
2021-10-06 10:39:05 +0200

    adding prototypes

--- v3/minitalk.h
+++ v3/minitalk.h
@@ -2,6 +2,7 @@
 # define MINITALK_H
 
 # include <signal.h>
+# include <unistd.h>
 
 typedef struct	s_msg
 {
@@ -12,6 +13,12 @@ typedef struct	s_msg
 typedef struct s_ack
 {
 	int	acked;
-}			t_ack
+}			t_ack;
+
+size_t	ft_strlen(const char *str);
+int ft_atoi(const char *nptr);
+void    ft_putchar(char c);
+void    ft_putnbr(int n);
+void    ft_putstr(char *s);
 
 #endif
---
+++ v3/utils.c
@@ -0,0 +1,74 @@
+#include "minitalk.h"
+
+int	ft_atoi(const char *nptr)
+{
+	size_t	i;
+	int		sign;
+	int		num;
+
+	i = 0;
+	sign = 1;
+	num = 0;
+	while ((nptr[i] > 8 && nptr[i] < 14) || nptr[i] == 32)
+		i++;
+	if (nptr[i] == 43 || nptr[i] == 45)
+	{
+		if (nptr[i] == 45)
+			sign *= -1;
+		i++;
+	}
+	while (nptr[i] > 47 && nptr[i] < 58)
+	{
+		num = num * 10 + nptr[i] - '0';
+		i++;
+	}
+	return (num * sign);
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int n)
+{
+	char	c;
+
+	if (n < 0)
+		write(1, "-", 1);
+	if (n == -2147483648)
+	{
+		write(1, "2", 1);
+		n = -147483648;
+	}
+	if (n < 0)
+		n *= -1;
+	if (n / 10 > 0)
+		ft_putnbr(n / 10);
+	c = n % 10 + '0';
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *s)
+{
+	size_t	i;
+
+	i = 0;
+	if (s == NULL)
+		return ;
+	while (s[i] != '\0')
+	{
+		write(1, &s[i], 1);
+		i++;
+	}
+}
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
