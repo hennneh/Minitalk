f2b22055d5cbe0078a4b6e85f01f6bc2fd98b971
hennneh <hendrik.lehmann96@gmail.com>
2021-10-07 15:50:05 +0200

    final version

---
+++ v3/client_bonus.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client_bonus.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hlehmann <hlehmann@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/13 16:54:48 by hlehmann          #+#    #+#             */
+/*   Updated: 2021/02/13 16:57:26 by hlehmann         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+t_ack	g_ack;
+
+void	send(int pid, char *msg, size_t len)
+{
+	int		shift;
+	size_t	i;
+
+	i = 0;
+	while (i <= len)
+	{
+		shift = 0;
+		while (shift <= 7)
+		{
+			g_ack.acked = 0;
+			if ((msg[i] >> shift) & 1)
+				kill(pid, SIGUSR2);
+			else
+				kill(pid, SIGUSR1);
+			shift++;
+			while (g_ack.acked == 0)
+				pause();
+		}
+		i++;
+	}
+}
+
+void	get_ack(int sigbit, siginfo_t *siginfo, void *context)
+{
+	(void) sigbit;
+	(void) siginfo;
+	(void) context;
+	g_ack.acked = 1;
+}
+
+int	main(int argc, char **argv)
+{
+	struct sigaction	sig;
+	int					pid;
+
+	g_ack.acked = 1;
+	sig.sa_sigaction = &get_ack;
+	sig.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &sig, NULL);
+	if (argc == 3)
+	{
+		pid = ft_atoi(argv[1]);
+		send(pid, argv[2], ft_strlen(argv[2]));
+	}
+	else
+		ft_putstr("Wrong input!\n");
+	return (0);
+}
---
+++ v3/server.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c	                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hlehmann <hlehmann@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/13 16:54:48 by hlehmann          #+#    #+#             */
+/*   Updated: 2021/02/13 16:57:26 by hlehmann         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+void	receive(int bit, siginfo_t *siginfo, void *context)
+{
+	static t_msg	msg;
+
+	if (bit == SIGUSR1)
+		msg.c += (0 << msg.size);
+	if (bit == SIGUSR2)
+		msg.c += (1 << msg.size);
+	msg.size++;
+	if (msg.size == 8)
+	{
+		ft_putchar(msg.c);
+		msg.c = 0;
+		msg.size = 0;
+	}
+	kill(siginfo->si_pid, SIGUSR1);
+	(void) context;
+}
+
+int	main(void)
+{
+	struct sigaction	sig;
+
+	sig.sa_sigaction = &receive;
+	sig.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &sig, NULL);
+	sigaction(SIGUSR2, &sig, NULL);
+	ft_putstr("PID: ");
+	ft_putnbr(getpid());
+	ft_putstr("\n");
+	while (1)
+		pause();
+}
